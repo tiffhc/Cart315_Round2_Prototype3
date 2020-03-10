af5fbe248c821a7848424d5ed9b9d363682d0a20
tiffhc <46658494+tiffhc@users.noreply.github.com>
2020-03-10 16:48:57 -0400

    AI agent cannot run together?

---
+++ Assets/Custom_Scripts/EnemyAI.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI; 
+
+public class EnemyAI : MonoBehaviour
+{
+    public NavMeshAgent monster; 
+    public GameObject destination; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        monster = this.GetComponent<NavMeshAgent>(); 
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //continously find them
+        monster.SetDestination(destination.transform.position);
+    }
+}
