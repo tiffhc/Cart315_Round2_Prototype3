9b6f8fd8c448eb6d49e767ef0e2b12a7f6dc65a3
tiffhc <46658494+tiffhc@users.noreply.github.com>
2020-03-13 18:33:50 -0400

    Restart button done!!!

--- Assets/Custom_Scripts/KillPlayer.cs
+++ Assets/Custom_Scripts/KillPlayer.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class KillPlayer : MonoBehaviour
 {
@@ -14,5 +15,14 @@ public class KillPlayer : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        Debug.Log("Ai killed player"); 
+        if(other.gameObject.CompareTag("Player"))
+        {
+            SceneManager.LoadScene("GameOver_Lose", LoadSceneMode.Single); 
+        }
     }
 }
