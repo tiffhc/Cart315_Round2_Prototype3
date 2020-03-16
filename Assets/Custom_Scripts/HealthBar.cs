0f40698d367872235f6bcd4128f13627281f04f6
Liu WenYue <LiuWY0912@gmail.com>
2020-03-16 15:26:54 -0400

    TP3: Added triggerzone and healthbar scripts

---
+++ Assets/Custom_Scripts/HealthBar.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+using System.Collections;
+
+public class HealthBar : MonoBehaviour
+{
+    public Image currentHealth;
+    public Text ratio;
+
+    private float hitpoint = 150f;
+    private float maxHitpoint = 150f;
+
+    private void Start()
+    {
+        UpdateHealthBar();
+    }
+
+    private void UpdateHealthBar()
+    {
+        float r = hitpoint / maxHitpoint;
+        currentHealth.rectTransform.localScale = new Vector3(r, 1, 1);
+        ratio.text = (r * 100).ToString("0") + '%';
+    }
+
+    public void TakeDamage(float damage)
+    {
+        hitpoint -= damage;
+        if (hitpoint < 0)
+        {
+            hitpoint = 0;
+            Debug.Log("Dead!");
+        }
+
+        UpdateHealthBar();
+    }
+
+    public void HealthDamage(float heal)
+    {
+        hitpoint += heal;
+        if (hitpoint > maxHitpoint)
+        {
+            hitpoint = maxHitpoint;
+        }
+
+        UpdateHealthBar();
+    }
+}
---
+++ Assets/Custom_Scripts/TriggerZone.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerZone : MonoBehaviour
+{
+    public bool isDamaging;
+    public float damage = 20f;
+
+    public void OnTriggerStay(Collider col)
+    {
+        if (col.tag == "Player")
+        {
+            col.gameObject.GetComponent<HealthBar>().TakeDamage(damage * Time.deltaTime);
+
+            //col.SendMessage((isDamaging) ? "TakeDamage" : "HealthDamage", Time.deltaTime * damage);
+        }
+    }
+}
